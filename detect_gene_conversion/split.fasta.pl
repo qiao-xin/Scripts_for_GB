7257fda8e883e6943b1482910e7b512939ce5546
Xin Qiao <qiaoxinqx2011@126.com>
2018-11-06 22:04:23 +0800

    Add files via upload

---
+++ detect_gene_conversion/split.fasta.pl
@@ -0,0 +1,16 @@
+use strict;
+use Bio::SeqIO;
+
+print " ========= Usage ======= \nperl split.fasta.pl inputfile outputdir\n=========================\n";
+
+my $fastafile = $ARGV[0];
+my $is = Bio::SeqIO -> new(-format=>'fasta', -file => $fastafile);
+
+while(my $seqobj = $is -> next_seq())
+{
+   my $outfile = $ARGV[1]."/".$seqobj->display_id().".fasta";
+   my $os = Bio::SeqIO -> new(-format => 'fasta', -file => ">".$outfile); 
+   $os -> write_seq($seqobj);
+}
+
+
